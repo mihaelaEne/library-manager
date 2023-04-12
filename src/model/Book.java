bd999bd42b6da9ce6bb4fb041ffbe0d271beeadc
ENE MIHAELA <mihaela.een@gmail.com>
2023-04-12 20:26:39 +0300

    added models

---
+++ src/model/Book.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Book {
+    private int id;
+    private int studentId;
+
+    private String title;
+
+    private String autor;
+
+    LocalDate createAt;
+
+
+    
+}
---
+++ src/model/Student.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Student {
+    String firstName;
+    String lastName;
+    String email;
+    int age;
+}
