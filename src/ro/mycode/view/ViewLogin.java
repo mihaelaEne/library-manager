97ad6521d866107a5905d3198572e774c8a215d6
ENE MIHAELA <mihaela.een@gmail.com>
2023-04-29 15:21:59 +0300

    finish taask-4

--- src/ro/mycode/view/ViewLogin.java
+++ src/ro/mycode/view/ViewLogin.java
@@ -1,2 +1,14 @@
-package ro.mycode.view;public class ViewLogin {
+package ro.mycode.view;
+
+import ro.mycode.controllers.BookController;
+import ro.mycode.model.Student;
+
+public class ViewLogin {
+
+    private Student student;
+    private BookController bookController;
+
+
+
+    private void login(int id, String lastN)a
 }
